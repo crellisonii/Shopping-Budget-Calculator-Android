588d166dee78ff62a79e94c9bf50f36c412ac6f2
CHARLES ELLISON II <crellisonii@yahoo.com>
2018-04-07 16:18:51 -0500

    fleshed out itemList.java

--- app/src/main/java/com/solodev/shoppingbudgetcalculator/services/models/ItemList.java
+++ app/src/main/java/com/solodev/shoppingbudgetcalculator/services/models/ItemList.java
@@ -4,6 +4,17 @@ import android.widget.CheckBox;
 
 public class ItemList{
 
+    // Database Table Name
+    private static final String TABLE = "items";
+
+    // Item table columns for this object
+    private static final String ID = "id";
+    private static final String NAME = "name";
+    private static final String SECTIONS = "section";
+    private static final String PRICE = "price";
+    private static final String QUANTITY = "quantity";
+
+    //
     private String id;
     private String name;
     private String section;
@@ -11,7 +22,7 @@ public class ItemList{
     private int quantity;
     private CheckBox checkBox;
 
-    public ItemList(String id, String name, String section, double price, int quantity){
+    private ItemList(String id, String name, String section, double price, int quantity){
         this.id = id;
         this.name = name;
         this.section = section;
@@ -57,5 +68,41 @@ public class ItemList{
 
     public void setQuantity(int quantity){
         this.quantity = quantity;
+    }
+
+    class Builder{
+
+        private String newId;
+        private String newName;
+        private String newSection;
+        private double newPrice;
+        private int newQuantity;
+
+        public Builder(String newId){}
+
+        public Builder name(String name){
+            this.newName = name;
+            return this;
+        }
+
+        public Builder section(String section){
+            this.newSection = section;
+            return this;
+        }
+
+        public Builder price(double price){
+            this.newPrice = price;
+            return this;
+        }
+
+        public Builder quantity(int quantity){
+            this.newQuantity = quantity;
+            return this;
+        }
+
+        public ItemList build(){
+            return new ItemList(newId, newName, newSection, newPrice, newQuantity);
+        }
+
     }
 }
