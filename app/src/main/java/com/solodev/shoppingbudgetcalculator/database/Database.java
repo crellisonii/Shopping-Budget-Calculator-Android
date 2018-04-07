039b164cc49cfe5b0fc7be04e5888a0825becb15
CHARLES ELLISON II <crellisonii@yahoo.com>
2018-04-07 15:17:22 -0500

    add database shell and refactor fragment name

---
+++ app/src/main/java/com/solodev/shoppingbudgetcalculator/database/Database.java
@@ -0,0 +1,29 @@
+package com.solodev.shoppingbudgetcalculator.database;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.solodev.shoppingbudgetcalculator.app.App;
+
+public class Database extends SQLiteOpenHelper{
+
+    private static final String DB_NAME = "shopping_budget.db";
+    private static final int DB_VERSION = 1;
+
+
+    public Database(){
+        super(App.getContext(), DB_NAME, null, DB_VERSION);
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db){
+
+    }
+
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
+
+    }
+}
--- app/src/main/java/com/solodev/shoppingbudgetcalculator/ui/ShoppingListViewFragment.java
+++ app/src/main/java/com/solodev/shoppingbudgetcalculator/ui/ShoppingListViewFragment.java
@@ -1,4 +1,4 @@
-package com.solodev.shoppingbudgetcalculator;
+package com.solodev.shoppingbudgetcalculator.ui;
 
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
@@ -6,12 +6,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.solodev.shoppingbudgetcalculator.R;
+
 /**
  * A placeholder fragment containing a simple view.
  */
-public class ShoppingListViewActivityFragment extends Fragment{
+public class ShoppingListViewFragment extends Fragment{
 
-    public ShoppingListViewActivityFragment(){
+    public ShoppingListViewFragment(){
     }
 
     @Override
